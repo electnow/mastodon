1535a11e27ebdbd7faaaf26484498f343610138b
Daniyal Abbasi <abbasi.daniyal98@gmail.com>
2023-10-01 04:45:25 +0530

    added leader profile without require a user account

--- db/migrate/20230923003005_add_electorate_to_accounts.rb
+++ db/migrate/20230923003005_add_electorate_to_accounts.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class AddElectorateToAccounts < ActiveRecord::Migration[7.0]
   def up
-    safety_assured do 
-      execute <<-SQL
+    safety_assured do
+      execute <<-SQL.squish
         ALTER TABLE accounts
         ADD electorate_id INTEGER NULL,
         ADD CONSTRAINT fk_accounts_electorates
@@ -13,8 +15,8 @@ class AddElectorateToAccounts < ActiveRecord::Migration[7.0]
   end
 
   def down
-    safety_assured do 
-      execute <<-SQL
+    safety_assured do
+      execute <<-SQL.squish
         ALTER TABLE accounts
         DROP CONSTRAINT IF EXISTS fk_accounts_electorates;
         ALTER TABLE accounts DROP electorate_id;
--- db/migrate/20230923013410_create_leader_profiles.rb
+++ db/migrate/20230923013410_create_leader_profiles.rb
@@ -1,16 +1,21 @@
-class CreateLeaderProfiles < ActiveRecord::Migration[7.0]
+# frozen_string_literal: true
 
+class CreateLeaderProfiles < ActiveRecord::Migration[7.0]
   def change
     create_table :leader_profiles do |t|
-      t.references :account, null: false, foreign_key: true
+      t.string :name
+      t.attachment :avatar
+      t.string :note, null: false, default: ''
       t.integer :type
       t.integer :level
-      t.references :parties, null: true, foreign_key: true
       t.integer :time_in_office
       t.integer :rebellions
       t.integer :votes_attended
       t.integer :votes_possible
       t.integer :offices
+
+      t.references :account, null: true, foreign_key: true
+      t.references :parties, null: true, foreign_key: true
 
       t.timestamps
     end
