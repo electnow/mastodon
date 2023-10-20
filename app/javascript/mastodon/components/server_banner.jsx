91d2f980ce88bc2d9400435daf4e508dce632b4c
sns0013 <sns0013@auburn.edu>
2023-10-20 11:25:36 +1100

    Update server_banner.jsx

--- app/javascript/mastodon/components/server_banner.jsx
+++ app/javascript/mastodon/components/server_banner.jsx
@@ -42,7 +42,7 @@ class ServerBanner extends PureComponent {
     return (
       <div className='server-banner'>
 
-        <ServerHeroImage blurhash={server.getIn(['thumbnail', 'blurhash'])} src={server.getIn(['thumbnail', 'url'])} className='server-banner__hero' />
+        
 
       </div>
     );
